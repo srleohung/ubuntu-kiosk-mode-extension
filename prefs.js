0cbc935fb54e0fa281130e8f366385641daa04d3
Leo Hung <leo@smartretail.co>
2023-03-24 17:39:20 +0800

    - Added text view - Added button for run command

--- prefs.js
+++ prefs.js
@@ -1,5 +1,4 @@
-const GObject = imports.gi.GObject;
-const Gtk = imports.gi.Gtk;
+const { Gtk, GLib, GObject } = imports.gi;
 
 function init() { }
 
@@ -22,6 +21,7 @@ const MyPrefsWidget = GObject.registerClass(
 
             this.connect('destroy', Gtk.main_quit);
 
+            // SpinButton
             let myLabel = new Gtk.Label({
                 label: "Translated Text"
             });
@@ -42,7 +42,35 @@ const MyPrefsWidget = GObject.registerClass(
             hBox.pack_start(myLabel, false, false, 0);
             hBox.pack_end(spinButton, false, false, 0);
 
+            // TextView
+            let textView = new Gtk.TextView();
+            textView.set_editable(false);
+            textView.set_cursor_visible(false);
+
+            let scrolledWindow = new Gtk.ScrolledWindow({
+                hscrollbar_policy: Gtk.PolicyType.NEVER,
+                vscrollbar_policy: Gtk.PolicyType.AUTOMATIC
+            });
+            scrolledWindow.set_size_request(300, 200);
+            scrolledWindow.add(textView);
+
+            // Button
+            let button = new Gtk.Button({
+                label: "ifconfig"
+            });
+
+            button.connect("clicked", function () {
+                let [success, stdout, stderr] = GLib.spawn_command_line_sync("ifconfig");
+                if (success) {
+                    textView.get_buffer().set_text(stdout.toString(), stdout.length);
+                } else {
+                    textView.get_buffer().set_text(stderr.toString(), stderr.length);
+                }
+            });
+
             this.add(hBox);
+            this.add(button);
+            this.add(scrolledWindow);
         }
 
     });
